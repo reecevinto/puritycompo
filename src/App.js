5c1863b9e70631e2a4420c256b169c518c1fcfbf
Collins Murgor <vintoncollins702@gmail.com>
2023-08-25 14:02:31 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+function Cup({ guest }) {
+  return <h2> Tea for guest #{guest}</h2>;
+}
+export default function TeaSet() {
+  return (
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
+  );
+}
